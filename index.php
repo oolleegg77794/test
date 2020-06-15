d122faee44e7e239a72357dc02221849e73ed887
oolleegg77794 <66882198+oolleegg77794@users.noreply.github.com>
2020-06-15 12:13:15 +0300

    Adaptive and hover "product block"

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>home page</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -22,8 +24,8 @@
 	<div class="sect1">
 	  	<div class="row">
 
-		    <div class="col-4">
-		    	<div class="left_panel">
+		    <div class="col-lg-4 col-md-6 order-sm-1 order-md-1 order-1 order-lg-0">
+		    	<div class="d-flex flex-column align-items-sm-center align-items-lg-start align-items-md-start">
 		    		<div class="el1">
 					 	<img src="img/user icon.png" id="user_icon">
 					 	<span>HELLO, YOU CAN</span>
@@ -42,42 +44,42 @@
 				</div>	
 		    </div>
 		    
-		    <div class="col-3"><img src="img/logo.png" style="margin-left: 12%;"></div>
+		    <div class="d-flex col-lg-3 col-md-12 justify-content-center align-items-center"><img src="img/logo.png"></div>
 		    
-		    <div class="col-5">
-
-		      	<div class="row justify-content-end">
-		      		<div class="r_el1">
-			    		<div class="col-md-auto"><img src="img/wishlist icon.png" id="wishlist_icon">WISHLIST:4</div>
+		    <div class="col-lg-5 col-md-6 order-md-1 order-sm-1 order-1">
+		    	<div class="d-flex flex-column align-items-sm-center align-items-lg-end align-items-md-end">
+			      	<div class="row justify-content-end">
+			      		<div class="r_el1">
+				    		<div class="col"><img src="img/wishlist icon.png" id="wishlist_icon">WISHLIST:4</div>
+				    	</div>
+				    	<div class="r_el2">
+			    			<div class="col"><img src="img/cart icon.png" id="cart_icon">SHOPPING CART: <span style="color:#cd222a; font-weight: bold;">$ 345.00</span></div>
+			    		</div>
 			    	</div>
-			    	<div class="r_el2">
-		    			<div class="col-md-auto"><img src="img/cart icon.png" id="cart_icon">SHOPPING CART: <span style="color:#cd222a; font-weight: bold;">$ 345.00</span></div>
-		    		</div>
-		    	</div>
 
-		    	<div class="row justify-content-end">			
-		    		<div class="col-md-12 col-lg-5">
-		    			<div class="good">
-		    				<div class="good_img"><img src="img/placehold.png" id="placehold_icon"></div>
-		    				<div class="good_text"><div class="good_text1">FLAT SHOES</div><div class="good_text2">$ 189.00</div></div>
-		    				<div class="good_close"><img src="img/x.png" id="x_icon"></div>
-		    			</div>
+			    	<div class="row d-flex flex-md-row align-items-sm-center align-items-lg-end align-items-md-end">			
+			    		<div class="col-md-6 col-lg-6 col-sm-6 ">
+			    			<div class="good">
+			    				<div class="good_img"><img src="img/placehold.png" id="placehold_icon"></div>
+			    				<div class="good_text"><div class="good_text1">FLAT SHOES</div><div class="good_text2">$ 189.00</div></div>
+			    				<div class="good_close"><img src="img/x.png" id="x_icon"></div>
+			    			</div>
 
-		    		</div>
-		    		<div class="col-md-12 col-lg-5">
-		    			<div class="good">
-		    				<div class="good_img"><img src="img/placehold.png" id="placehold_icon"></div>
-		    				<div class="good_text"><div class="good_text1">FLAT SHOES</div><div class="good_text2">$ 189.00</div></div>
-		    				<div class="good_close"><img src="img/x.png" id="x_icon"></div>
-		    			</div>
+			    		</div>
+			    		<div class="col-md-6 col-lg-6 col-sm-6 ">
+			    			<div class="good">
+			    				<div class="good_img"><img src="img/placehold.png" id="placehold_icon"></div>
+			    				<div class="good_text"><div class="good_text1">FLAT SHOES</div><div class="good_text2">$ 189.00</div></div>
+			    				<div class="good_close"><img src="img/x.png" id="x_icon"></div>
+			    			</div>
+			    		</div>
 		    		</div>
 	    		</div>
-
-	    		<div class="row justify-content-end">
-		    		<div class="col-md-auto" style="padding: 0 0;">
+	    		<div class="d-flex flex-sm-column flex-lg-row flex-xl-row align-items-sm-center  align-items-md-end align-items-lg-end align-items-xl-end justify-content-xl-end justify-content-lg-end">
+		    		<div class="col-auto" style="padding: 0 0;">
 						<button id="btn_1"><img src="img/plus icon copy.png"> SHOW ALL PRODUCTS</button>
 		    		</div>
-		    		<div class="col-md-auto" style="padding: 0 0;">
+		    		<div class="col-auto" style="padding: 0 0;">
 						<button id="btn_2"><img src="img/checkout icon.png"> CHECK OUT</button>
 		    		</div>
 				</div>	
@@ -100,114 +102,164 @@
 			<div class="line"></div>	
 	</div>
 	<div class="sect3">		
-		<div class="row justify-content-center" style="margin: auto;">			
-			<div class="col-lg-6 col-md-12">
-				<div class="row justify-content-end">
-					<div class="col-md-auto" id="photo_1">
-						<div id="el_photo_1">
-							<div style="text-align: center; font-weight:bold; font-size: 30pt; ">MAY</div>
-							<div style="text-align: center; font-size: 18pt; ">Collection</div>
-							<button id="btn_el_photo_1" type="button" class="register">See Products</button>
+		
+			<div class="row justify-content-center align-items-sm-center align-items-xl-end" style="margin: auto;">			
+				<div class="col-lg-12 col-md-12 col-xl-6" style="padding-left: 0px; padding-right: 0px;">
+					<div class="row justify-content-xl-end justify-content-sm-center">
+						<div class="col-md-auto col-sm-auto" id="photo_1">
+							<div id="el_photo_1">
+								<div style="text-align: center; font-weight:bold; font-size: 30pt; ">MAY</div>
+								<div style="text-align: center; font-size: 18pt; ">Collection</div>
+								<button id="btn_el_photo_1" type="button" class="register">See Products</button>
+							</div>
+						</div>
+						<div class="col-md-auto col-sm-auto">
+							<div class="col" id="photo_4"><img src="img/4.png"></div>
+							<div class="col" id="photo_2"><img src="img/2.png"></div>
 						</div>
 					</div>
-					<div class="col-md-auto">
-						<div class="col" id="photo_4"><img src="img/4.png"></div>
-						<div class="col" id="photo_2"><img src="img/2.png"></div>
+				</div>
+				<div class="d-flex flex-column justify-content-xl-start col-xl-6 col-lg-12 col-md-12 justify-content-md-center  align-items-md-center align-items-xl-start align-items-sm-center"style="padding-left: 0px; padding-right: 0px;">
+					<div class="col-auto " id="photo_3">
+						<div id="el_photo_3">
+							<div style="text-align: center; font-weight:bold; font-size: 30pt; ">MAY</div>
+							<div style="text-align: center; font-size: 18pt; ">Collection</div>
+							<button id="btn_el_photo_3" type="button" class="register">See Products</button>
+						</div>
 					</div>
+					<div class="col-auto" id="photo_5"><img src="img/5.png"></div>
 				</div>
 			</div>
-			<div class="col-lg-6 col-md-12">
-				<div class="col-md-auto" id="photo_3">
-					<div id="el_photo_3">
-						<div style="text-align: center; font-weight:bold; font-size: 30pt; ">MAY</div>
-						<div style="text-align: center; font-size: 18pt; ">Collection</div>
-						<button id="btn_el_photo_3" type="button" class="register">See Products</button>
-					</div>
-				</div>
-				<div class="col" id="photo_5"><img src="img/5.png"></div>
-			</div>
-		</div>			
+					
 	</div>
 	<div class="sect4">
 		<div class="row justify-content-center">			
-			<div class="col-md-auto" id="FEATURED"><span style="font-weight: bold; font-size: 48pt;">FEATURED</span><img src="img/arrow down.png" style="margin-bottom: 25px; margin-left: 15px;"></div>
-			<div class="col-md-auto" id="ARRIVALS"><span style="font-weight: lighter; font-size: 48pt;">ARRIVALS</span><img src="img/arrow down.png" style="margin-bottom: 25px; margin-left: 15px;"></div>		
+			<div class="col-auto" id="FEATURED"><span style="font-weight: bold; font-size: 48pt;">FEATURED</span><img src="img/arrow down.png" style="margin-bottom: 25px; margin-left: 15px;"></div>
+			<div class="col-auto" id="ARRIVALS"><span style="font-weight: lighter; font-size: 48pt;">ARRIVALS</span><img src="img/arrow down.png" style="margin-bottom: 25px; margin-left: 15px;"></div>		
 		</div>
 		<div style="text-align:center;"><span>“Whoever said money can’t buy happiness simply didn’t know WHERE TO SHOP”</span></div>
 		
 		<div class="products">
 			<div class="row justify-content-center">
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
+				<div id="container_box" class="col-auto"style="padding-left: 0px; padding-right: 0px;">
 					<div class="box">
 						<img src="img/pr1.png">
 						<hr id="line2">
 						<div class="pr_name">BLAZER SLIMFIT</div>
 						<div class="pr_price">$279,00</div>
+						<div class="cart_hover">
+							<button type="button" class="btn_hover1">DETAIL</button>
+							<button type="button" class="btn_hover2">ADD TO CART</button>
+							<div class="hr_hover"></div>
+							<div class="wishlist_hover"><img src="img/wishlist icon.png" id="wishlist_icon"><span>ADD TO WISHLIST</span></div>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
+				<div id="container_box" class="col-auto"style="padding-left: 0px; padding-right: 0px;">
 					<div class="box">
 						<img src="img/pr2.png">
 						<hr id="line2">
 						<div class="pr_name">SLIM PANTS</div>
 						<div class="pr_price">$279,00</div>
+						<div class="cart_hover">
+							<button type="button" class="btn_hover1">DETAIL</button>
+							<button type="button" class="btn_hover2">ADD TO CART</button>
+							<div class="hr_hover"></div>
+							<div class="wishlist_hover"><img src="img/wishlist icon.png" id="wishlist_icon"><span>ADD TO WISHLIST</span></div>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
+				<div id="container_box" class="col-auto"style="padding-left: 0px; padding-right: 0px;">
 					<div class="box">
 						<img src="img/pr3.png">
 						<hr id="line2">
 						<div class="pr_name">SWEATSHIRT</div>
 						<div class="pr_price">$279,00</div>
+						<div class="cart_hover">
+							<button type="button" class="btn_hover1">DETAIL</button>
+							<button type="button" class="btn_hover2">ADD TO CART</button>
+							<div class="hr_hover"></div>
+							<div class="wishlist_hover"><img src="img/wishlist icon.png" id="wishlist_icon"><span>ADD TO WISHLIST</span></div>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
+				<div id="container_box" class="col-auto"style="padding-left: 0px; padding-right: 0px;">
 					<div class="box">
 						<img src="img/pr4.png">
 						<hr id="line2">
 						<div class="pr_name">FORMAL BLOUSE</div>
 						<div class="pr_price">$279,00</div>
+						<div class="cart_hover">
+							<button type="button" class="btn_hover1">DETAIL</button>
+							<button type="button" class="btn_hover2">ADD TO CART</button>
+							<div class="hr_hover"></div>
+							<div class="wishlist_hover"><img src="img/wishlist icon.png" id="wishlist_icon"><span>ADD TO WISHLIST</span></div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="row justify-content-center">
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
+			<div class="row justify-content-center" style="padding-bottom: 30px;">
+				<div id="container_box" class="col-auto"style="padding-left: 0px; padding-right: 0px;">
 					<div class="box">
 						<img src="img/pr5.png">
 						<hr id="line2">
 						<div class="pr_name">WOMEN ARMY PANTS</div>
 						<div class="pr_price">$279,00</div>
+						<div class="cart_hover">
+							<button type="button" class="btn_hover1">DETAIL</button>
+							<button type="button" class="btn_hover2">ADD TO CART</button>
+							<div class="hr_hover"></div>
+							<div class="wishlist_hover"><img src="img/wishlist icon.png" id="wishlist_icon"><span>ADD TO WISHLIST</span></div>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
+				<div id="container_box" class="col-auto"style="padding-left: 0px; padding-right: 0px;">
 					<div class="box">
 						<img src="img/pr6.png">
 						<hr id="line2">
 						<div class="pr_name">SUMMER PANTS</div>
 						<div class="pr_price">$279,00</div>
+						<div class="cart_hover">
+							<button type="button" class="btn_hover1">DETAIL</button>
+							<button type="button" class="btn_hover2">ADD TO CART</button>
+							<div class="hr_hover"></div>
+							<div class="wishlist_hover"><img src="img/wishlist icon.png" id="wishlist_icon"><span>ADD TO WISHLIST</span></div>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
+				<div id="container_box" class="col-auto"style="padding-left: 0px; padding-right: 0px;">
 					<div class="box">
 						<img src="img/pr7.png">
 						<hr id="line2">
 						<div class="pr_name">COLLAR CHIFFON</div>
 						<div class="pr_price">$279,00</div>
+						<div class="cart_hover">
+							<button type="button" class="btn_hover1">DETAIL</button>
+							<button type="button" class="btn_hover2">ADD TO CART</button>
+							<div class="hr_hover"></div>
+							<div class="wishlist_hover"><img src="img/wishlist icon.png" id="wishlist_icon"><span>ADD TO WISHLIST</span></div>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
+				<div id="container_box" class="col-auto"style="padding-left: 0px; padding-right: 0px;">
 					<div class="box">
 						<img src="img/pr8.png">
 						<hr id="line2">
 						<div class="pr_name">SLIM JEANS</div>
 						<div class="pr_price">$279,00</div>
+						<div class="cart_hover">
+							<button type="button" class="btn_hover1">DETAIL</button>
+							<button type="button" class="btn_hover2">ADD TO CART</button>
+							<div class="hr_hover"></div>
+							<div class="wishlist_hover"><img src="img/wishlist icon.png" id="wishlist_icon"><span>ADD TO WISHLIST</span></div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="more_product"  style="padding-top: 35px;">
-			<img src="img/plus icon copy.png" style="filter: invert(1); width: 20px; padding-bottom: 6px;">
-			LOAD MORE PRODUCTS
+			<a id="a_load_more_product" href="#"><img src="img/plus icon copy.png" style="filter: invert(1); width: 20px; padding-bottom: 6px;">
+			LOAD MORE PRODUCTS</a>
 		</div>
 	</div>
 	<div class="sect5">
@@ -215,12 +267,12 @@
 			<div class="categori_text">CATEGORI</div>
 			<div class="categori_menu">
 				<div class="row justify-content-center">
-		  			<div class="col-md-auto"><a class="nav-link" href="#">JACKETS</a></div>
-		  			<div class="col-md-auto"><a class="nav-link" href="#">T-SHIRTS</a></div>
-		  			<div class="col-md-auto"><a class="nav-link" href="#">SKIRTS</a></div>
-		  			<div class="col-md-auto"><a class="nav-link" href="#">PANTS</a></div>
-		  			<div class="col-md-auto"><a class="nav-link" href="#">SHOES</a></div>
-		  			<div class="col-md-auto"><a class="nav-link" href="#">ACCESSORIES</a></div>
+		  			<div class="col-auto"><a class="nav-link" href="#">JACKETS</a></div>
+		  			<div class="col-auto"><a class="nav-link" href="#">T-SHIRTS</a></div>
+		  			<div class="col-auto"><a class="nav-link" href="#">SKIRTS</a></div>
+		  			<div class="col-auto"><a class="nav-link" href="#">PANTS</a></div>
+		  			<div class="col-auto"><a class="nav-link" href="#">SHOES</a></div>
+		  			<div class="col-auto"><a class="nav-link" href="#">ACCESSORIES</a></div>
 				</div>
 			</div>
 		</div>
@@ -231,32 +283,32 @@
 		</div>
 		<div class="latest_viewed_products">
 			<div class="row justify-content-center">	
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
-					<div class="box">
+				<div class="col-auto"style="padding-left: 0px; padding-right: 0px;">
+					<div class="box2">
 						<img src="img/pr4.png">
 						<hr id="line2">
 						<div class="pr_name">FORMAL BLOUSE</div>
 						<div class="pr_price">$279,00</div>
 					</div>
 				</div>			
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
-					<div class="box">
+				<div class="col-auto"style="padding-left: 0px; padding-right: 0px;">
+					<div class="box2">
 						<img src="img/pr4.png">
 						<hr id="line2">
 						<div class="pr_name">FORMAL BLOUSE</div>
 						<div class="pr_price">$279,00</div>
 					</div>
 				</div>				
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
-					<div class="box">
+				<div class="col-auto"style="padding-left: 0px; padding-right: 0px;">
+					<div class="box2">
 						<img src="img/pr4.png">
 						<hr id="line2">
 						<div class="pr_name">FORMAL BLOUSE</div>
 						<div class="pr_price">$279,00</div>
 					</div>
 				</div>
-				<div class="col-md-auto"style="padding-left: 0px; padding-right: 0px;">
-					<div class="box">
+				<div class="col-auto"style="padding-left: 0px; padding-right: 0px;">
+					<div class="box2">
 						<img src="img/pr4.png">
 						<hr id="line2">
 						<div class="pr_name">FORMAL BLOUSE</div>
@@ -266,19 +318,18 @@
 			</div>
 		</div>		
 	</div>
-	<div class="footer_hr" style="margin: auto; margin-top: 100px; position: relative;"><hr><img src="img/logo copy.png" id="footer_image"></div>
+	<div class="footer_hr"><hr><img src="img/logo copy.png" id="footer_image"></div>
 	<div class="footer">
 		<div class="row justify-content-center">
-			<div class="col-md-auto">
-				<div class="headertext_foter">CONNECT WITH US</div>
+			<div class="col-auto">
+				<div class="headertext_foter d-flex justify-content-sm-center justify-content-md-start">CONNECT WITH US</div>
 				<hr style="width: 265px;">
-				<div>Kebon jeruk, Jakarta, ID</div>
-				<div>+88  888 888 888</div>
-				<div>fashionholic@davelyn.com</div>
-
+				<div class="d-flex justify-content-sm-center justify-content-md-start">Kebon jeruk, Jakarta, ID</div>
+				<div class="d-flex justify-content-sm-center justify-content-md-start">+88  888 888 888</div>
+				<div class="d-flex justify-content-sm-center justify-content-md-start">fashionholic@davelyn.com</div>
 			</div>	
-			<div class="col-md-auto">
-				<div class="headertext_foter">HOLIDAY SALES</div>
+			<div class="col-auto">
+				<div class="headertext_foter d-flex justify-content-sm-center justify-content-md-start">HOLIDAY SALES</div>
 				<hr style="width: 265px;">
 				<div class="row">
     				<div class="col-4"><img src="img/placehold_footer.png" id="placehold_footer_icon"></div>
@@ -289,8 +340,8 @@
     				<div class="col-8"style="padding: 0px 0px;">GUCCI - FASHION BAG<p>$ 156.00</p></div>	
     			</div>
 			</div>
-			<div class="col-md-auto">
-				<div class="headertext_foter">NEW ARRIVALS</div>
+			<div class="col-auto">
+				<div class="headertext_foter d-flex justify-content-sm-center justify-content-md-start">NEW ARRIVALS</div>
 				<hr style="min-width: 265px;">
 				<div class="row">
     				<div class="col-4"><img src="img/placehold_footer.png" id="placehold_footer_icon"></div>
@@ -301,8 +352,8 @@
     				<div class="col-8"style="padding: 0px 0px;">VICTORIA S - LINGERIE<p>$ 156.00</p></div>
     			</div>
 			</div>
-			<div class="col-md-auto">
-				<div class="headertext_foter">OUR SUPPORT</div>
+			<div class="col-auto">
+				<div class="headertext_foter d-flex justify-content-sm-center justify-content-md-start">OUR SUPPORT</div>
 				<hr style="width: 265px;">
 				<div class="footer_links">
 					<a href="#" id="link_footer">- Terms & Conditions</a>
@@ -327,5 +378,8 @@
 
 
 </div>
+
+
+
 </body>
 </html>
